8dfe8fef7200b3f0e9daae0cd62f8c2ebfe3e18d
Tedros Kahsu <tedkahcom@gmail.com>
2023-12-26 12:23:59 +0300

    Fix e.preventDefault issue

--- js/script.js
+++ js/script.js
@@ -15,26 +15,48 @@ btnNavEl.addEventListener("click", function () {
 const allLinks = document.querySelectorAll("a:link");
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
-    e.preventDefault();
     const href = link.getAttribute("href");
     //scroll back to top
-    if (href === "#")
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
+    if (href === "#") e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
     //scroll to other links
     if (href !== "#" && href.startsWith("#")) {
+      e.preventDefault();
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
 
     //close mobile navigation
     if (link.classList.contains("main-nav-link")) {
+      e.preventDefault();
       headerEl.classList.toggle("nav-open");
+    }
+    // Manually navigate if none of the custom conditions match
+    if (!e.defaultPrevented) {
+      window.location.href = href;
     }
   });
 });
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.querySelector(".header").classList.add("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+  }
+);
+observer.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
